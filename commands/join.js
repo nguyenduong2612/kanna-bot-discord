36dd5b6e9d5eae19e1dff49ed7af0b5851854922
nguyenduong2612 <nguyenduong261298@gmail.com>
2020-08-10 15:56:54 +0700

    test join

---
+++ commands/join.js
@@ -0,0 +1,22 @@
+const join = (message, args) => {
+  const { channel } = message.member.voice;
+  channel.join().then(connection => {
+        // Yay, it worked!
+    console.log("Successfully connected.");
+     message.channel.send(`connected`)
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+    message.channel.send(`Could not join the channel: ${e}`)
+  });
+}
+
+module.exports = {
+	name: 'join',
+	aliases: ['j'],
+  description: 'kanna react',
+  guildOnly: true,
+	execute(message, args) {
+    join(message, args)
+	},
+};
